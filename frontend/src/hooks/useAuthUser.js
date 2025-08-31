9bb079be0ff1e9aa6ec9b2f60f3d0cd0643058cc
spandana <spandanaputti43@gmail.com>
2025-08-31 10:45:43 +0530

    Initial commit

---
+++ frontend/src/hooks/useAuthUser.js
@@ -0,0 +1,18 @@
+
+import { useQuery } from '@tanstack/react-query';
+import { getAuthUser } from '../lib/api.js';
+
+const useAuthUser = () => {
+    const authUser = useQuery({
+        queryKey:["authUser"],
+        queryFn: getAuthUser,
+        retry: false,
+      });
+
+      return {isLoading: authUser.isLoading, authUser:authUser.data?.user};
+  
+}
+
+
+
+export default useAuthUser
